9a84d13bc2675e525c78d0e9f05caccdc34aaa90
Szczenathemonke <mpilkowski.learn@gmail.com>
2023-04-13 15:01:59 +0200

    pixisandbox

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,71 @@
-import { Application, Sprite } from 'pixi.js'
+import {
+  Application,
+  Container,
+  Graphics,
+  Sprite,
+  TextStyle,
+  Text,
+  BlurFilter,
+  ParticleContainer,
+  Texture,
+} from "pixi.js";
+import * as particleSettings from "./emitter.json";
+import { Emitter } from "pixi-particles";
 
 const app = new Application({
-	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+  view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+  resolution: window.devicePixelRatio || 1,
+  autoDensity: true,
+  backgroundColor: 0x6495ed,
+  width: window.innerWidth,
+  height: window.innerHeight,
 });
+const myBlurFilter = new BlurFilter();
+
+const conty: Container = new Container();
+conty.x = 200;
+conty.y = 0;
+app.stage.addChild(conty);
 
 const clampy: Sprite = Sprite.from("clampy.png");
+clampy.x = 100;
+clampy.y = 100;
+clampy.filters = [myBlurFilter];
+conty.addChild(clampy);
 
-clampy.anchor.set(0.5);
+const graphy: Graphics = new Graphics();
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+// we give instructions in order. begin fill, line style, draw circle, end filling
+graphy.beginFill(0xff00ff);
+graphy.lineStyle(10, 0x00ff00);
+graphy.drawCircle(0, 0, 25); // See how I set the drawing at 0,0? NOT AT 100, 100!
+graphy.endFill();
 
-app.stage.addChild(clampy);
+conty.addChild(graphy); //I can add it before setting position, nothing bad will happen.
+
+// Here we set it at 100,100
+graphy.x = 100;
+graphy.y = 100;
+
+const styly: TextStyle = new TextStyle({
+  align: "center",
+  fill: "#754c24",
+  fontSize: 42,
+});
+
+const texty: Text = new Text("text", styly); // Text supports unicode!
+texty.text = "This is expensive to change, please do not abuse";
+
+conty.addChild(texty);
+
+const particleContainer = new ParticleContainer();
+app.stage.addChild(particleContainer);
+
+const emitter = new Emitter(
+  particleContainer,
+  Texture.from("particle.png"),
+  particleSettings
+);
+emitter.autoUpdate = true; // If you keep it false, you have to update your particles yourself.
+emitter.updateSpawnPos(200, 100);
+emitter.emit = true;
