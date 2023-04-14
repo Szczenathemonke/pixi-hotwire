e216a10aebbabee148fdf6fe38b6e93f7c9611d0
Szczenathemonke <mpilkowski.learn@gmail.com>
2023-04-14 15:28:55 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,24 @@
-import {
-  Application,
-  Container,
-  Graphics,
-  Sprite,
-  TextStyle,
-  Text,
-  BlurFilter,
-  ParticleContainer,
-  Texture,
-} from "pixi.js";
-import * as particleSettings from "./emitter.json";
+import { Application, ParticleContainer, Texture } from "pixi.js";
+import * as particleSettings from "./emitter1.json";
 import { Emitter } from "pixi-particles";
 
 const app = new Application({
   view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
   resolution: window.devicePixelRatio || 1,
   autoDensity: true,
-  backgroundColor: 0x6495ed,
+  backgroundColor: "00AAFF",
   width: window.innerWidth,
   height: window.innerHeight,
 });
-const myBlurFilter = new BlurFilter();
-
-const conty: Container = new Container();
-conty.x = 200;
-conty.y = 0;
-app.stage.addChild(conty);
-
-const clampy: Sprite = Sprite.from("clampy.png");
-clampy.x = 100;
-clampy.y = 100;
-clampy.filters = [myBlurFilter];
-conty.addChild(clampy);
-
-const graphy: Graphics = new Graphics();
-
-// we give instructions in order. begin fill, line style, draw circle, end filling
-graphy.beginFill(0xff00ff);
-graphy.lineStyle(10, 0x00ff00);
-graphy.drawCircle(0, 0, 25); // See how I set the drawing at 0,0? NOT AT 100, 100!
-graphy.endFill();
-
-conty.addChild(graphy); //I can add it before setting position, nothing bad will happen.
-
-// Here we set it at 100,100
-graphy.x = 100;
-graphy.y = 100;
-
-const styly: TextStyle = new TextStyle({
-  align: "center",
-  fill: "#754c24",
-  fontSize: 42,
-});
-
-const texty: Text = new Text("text", styly); // Text supports unicode!
-texty.text = "This is expensive to change, please do not abuse";
-
-conty.addChild(texty);
 
 const particleContainer = new ParticleContainer();
+particleContainer.x = 200;
+particleContainer.y = 200;
 app.stage.addChild(particleContainer);
 
 const emitter = new Emitter(
   particleContainer,
-  Texture.from("particle.png"),
+  Texture.from("szczonke.png"),
   particleSettings
 );
 emitter.autoUpdate = true; // If you keep it false, you have to update your particles yourself.
